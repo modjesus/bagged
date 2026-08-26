b02b78304c78c11ef0587e1701f2593ce10b0811
modjesus <joesullivan1998@me.com>
2026-08-26 23:02:51 +0100

    Update Firebase configuration format

--- firebase-config.js
+++ firebase-config.js
@@ -20,5 +20,11 @@
 // Leave this file exactly as it is if you are not using accounts.
 // ---------------------------------------------------------------------------
 
-window.FIREBASE_CONFIG = {
+window.FIREBASE_CONFIG = {const firebaseConfig = {
+  apiKey: "AIzaSyAgc2oR0gWvITCwjDjBACXzBCiEzdaxoT4",
+  authDomain: "bagged-287c0.firebaseapp.com",
+  projectId: "bagged-287c0",
+  storageBucket: "bagged-287c0.firebasestorage.app",
+  messagingSenderId: "796045397214",
+  appId: "1:796045397214:web:746d429a695ee6db25c64f"
 };
